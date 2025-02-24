27c2ae3d4a89b554e0b54c834b42dfc762f96fee
Sean Kim <sean.kim0327@gmail.com>
2025-02-23 20:24:50 -0500

    home page fonts and widths more mobile-friendly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,17 @@
         <Avatar.Fallback>sk</Avatar.Fallback>
     </Avatar.Root>
     
-    <h1 class="text-left font-bold text-4xl w-3/4 mt-8">
+    <h1 class="text-left font-bold text-2xl sm:text-4xl w-full sm:w-3/4 mt-8">
         Hi! I'm Sean (Taemin/태민) Kim. I'm a software engineer with a passion for NLP.
     </h1>
     <div class="flex flex-col gap-1">
-        <h3 class="font-semibold text-muted-foreground">
+        <h3 class="font-semibold text-sm sm:text-base text-muted-foreground">
             CS @ Rutgers University-New Brunswick (2026)
         </h3>
-        <h3 class="font-semibold text-muted-foreground">
+        <h3 class="font-semibold text-sm sm:text-base text-muted-foreground">
             Incoming SWE Intern @ ... (2025)
         </h3>
-        <h3 class="font-semibold text-muted-foreground">
+        <h3 class="font-semibold text-sm sm:text-base text-muted-foreground">
             SWE Intern @ ACL Agency LLC (2024)
         </h3>
     </div>
