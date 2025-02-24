02d496c06854aa8143686f245a812ac7e36202c7
Sean Kim <sean.kim0327@gmail.com>
2025-02-24 00:04:26 -0500

    deploy fail... hmm

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
 <div class="container flex flex-col items-start gap-2">
     <h1 class="text-4xl font-bold">Error</h1>
-    <p class="text-lg">This page does not exist. You were clever enough to know that, right?</p>
+    <p class="text-lg">This page does not exist. You're clever enough to know that, right?</p>
 </div>
